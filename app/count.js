9dc0976e7dd4570d6a3cbc736400342e2115cd65
dayaRoman <semfaro957@gmail.com>
2022-04-01 11:31:10 +0300

    bestPractices, count, flowControl tests

--- app/count.js
+++ app/count.js
@@ -1,7 +1,22 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+    count: function (start, end) {
+      let timer;
+      function tick() {
+        if (start <= end) {
+          console.log(start);
+          timer = setTimeout(tick, 100);
+          start++;
+        }
+      }
 
-  }
+      tick();
+
+      return {
+        cancel () {
+          clearTimeout(timer);
+        }
+      }
+    },
 };
