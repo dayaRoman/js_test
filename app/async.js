b344643dc84bfbc5db2e8a48d4965c3eec2c0e0c
dayaRoman <semfaro957@gmail.com>
2022-04-01 10:11:06 +0300

    async test

--- app/async.js
+++ app/async.js
@@ -1,11 +1,21 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+    async: function (value) {
+        return new Promise(function (resolve) {
+            resolve(value);
+        });
+    },
 
-  },
-
-  manipulateRemoteData: function(url) {
-
-  }
+    manipulateRemoteData: function (url) {
+        return fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                let result = [];
+                for (let i = 0; i < data.people.length; i++) {
+                    result[i] = data.people[i].name;
+                }
+                return result.sort();
+            });
+    },
 };
